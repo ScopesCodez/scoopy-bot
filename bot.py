8751ed3894c503c81b660ee195895f68acc10176
ELCScopes <seif.wessam66@gmail.com>
2021-07-02 12:00:31 +0200

    Added loading files in cog folder when bot is on

--- bot.py
+++ bot.py
@@ -11,4 +11,13 @@ bot = commands.Bot(command_prefix="-",
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
+for cog in os.listdir(r"cogs"):
+    if cog.endswith(".py"):
+        try:
+            cog = f"cogs.{cog.replace('.py', '')}"
+            bot.load_extension(cog)
+        except Exception as e:
+            print(f"{cog} is failed to load:")
+            raise e
+
 bot.run(config("TOKEN"))
