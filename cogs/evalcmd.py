833911f022db740d5f8754a1c0adb6dfed0459c4
ELCScopes <seif.wessam66@gmail.com>
2021-07-06 05:20:11 +0200

    Added eval command (copy n' paste it and u got an eval cmd)

---
+++ cogs/evalcmd.py
@@ -0,0 +1,79 @@
+import discord
+import discord
+from discord.ext import commands
+from traceback import format_exception
+import io
+import textwrap
+import contextlib
+from discord.ext.buttons import Paginator
+
+
+class Pag(Paginator):
+    async def teardown(self):
+        try:
+            await self.page.clear_reactions()
+        except discord.HTTPException:
+            pass
+
+
+def clean_code(content):
+    if content.startswith("```") and content.endswith("```"):
+        return "\n".join(content.split("\n")[1:])[:-3]
+    else:
+        return content
+
+
+class EvalCMD(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="eval", aliases=["exec"])
+    @commands.has_role(861807985231134722)
+    async def _eval(self, ctx, *, code):
+        code = clean_code(code)
+
+        local_variables = {
+            "discord": discord,
+            "commands": commands,
+            "client": self.bot,
+            "bot": self.bot,
+            "ctx": ctx,
+            "channel": ctx.channel,
+            "author": ctx.author,
+            "guild": ctx.guild,
+            "message": ctx.message
+        }
+
+        stdout = io.StringIO()
+
+        try:
+            with contextlib.redirect_stdout(stdout):
+                exec(
+                    f"async def func():\n{textwrap.indent(code, '    ')}", local_variables,
+                )
+
+                obj = await local_variables["func"]()
+                result = f"{stdout.getvalue()}\n-- {obj}\n"
+        except Exception as e:
+            result = "".join(format_exception(e, e, e.__traceback__))
+
+        pager = Pag(
+            timeout=100,
+            entries=[result[i: i + 2000] for i in range(0, len(result), 2000)],
+            length=1,
+            prefix="```py\n",
+            suffix="```"
+        )
+
+        await pager.start(ctx)
+
+    @_eval.error
+    async def on_command_error(self, ctx, error):
+        embed = discord.Embed(color=discord.Color.red(),
+                              description="You need the role **eval perms** to use the eval command.")
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(EvalCMD(bot))
